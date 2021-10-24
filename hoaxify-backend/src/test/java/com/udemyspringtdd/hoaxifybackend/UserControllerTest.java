fc59a83931f467dba3a00ca2ff47ff3691e1d04a
santosjordi <jordiws@gmail.com>
2021-10-24 17:27:09 -0300

    update

--- hoaxify-backend/src/test/java/com/udemyspringtdd/hoaxifybackend/UserControllerTest.java
+++ hoaxify-backend/src/test/java/com/udemyspringtdd/hoaxifybackend/UserControllerTest.java
@@ -1,16 +1,22 @@
 package com.udemyspringtdd.hoaxifybackend;
 
-import com.udemyspringtdd.hoaxifybackend.user.User;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.udemyspringtdd.hoaxifybackend.user.User;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class UserControllerTest {
 
@@ -24,6 +30,13 @@ public class UserControllerTest {
         user.setDisplayName("test-display");
         user.setPassword("P4ssword");
 
+        ResponseEntity<Object> response = testRestTemplate.postForEntity("/api/1.0/users", user, Object.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    public void test(){
 
     }
 }

596c6298ea970dff147a6a21245f836e0065fdbe
santosjordi <jordiws@gmail.com>
2021-10-25 21:58:57 -0300

    back+front

--- hoaxify-backend/src/main/java/com/udemyspringtdd/hoaxifybackend/user/User.java
+++ hoaxify-backend/src/main/java/com/udemyspringtdd/hoaxifybackend/user/User.java
@@ -8,8 +8,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
 
-@Data
 @Entity
+@Data
 public class User implements Serializable {
 
     @Id
--- hoaxify-backend/src/test/java/com/udemyspringtdd/hoaxifybackend/UserControllerTest.java
+++ hoaxify-backend/src/test/java/com/udemyspringtdd/hoaxifybackend/UserControllerTest.java
@@ -2,6 +2,8 @@ package com.udemyspringtdd.hoaxifybackend;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.udemyspringtdd.hoaxifybackend.user.UserRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,16 +23,30 @@ import com.udemyspringtdd.hoaxifybackend.user.User;
 public class UserControllerTest {
 
     public static final String API_1_0_USERS = "/api/1.0/users";
+
     @Autowired
     TestRestTemplate testRestTemplate;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Before
+    public void cleanUp(){
+        userRepository.deleteAll();
+    }
 
     @Test
     public void postUser_whenUserIsValid_receiveOK(){
         User user = createValidUser();
-
         ResponseEntity<Object> response = testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
-
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    public void postUser_whenUserIsValid_postUserToDataBase(){
+        User user = createValidUser();
+        testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
+        assertThat(userRepository.count()).isEqualTo(1);
     }
 
     private User createValidUser() {
@@ -39,12 +55,5 @@ public class UserControllerTest {
         user.setDisplayName("test-display");
         user.setPassword("P4ssword");
         return user;
-    }
-
-    @Test
-    public void postUser_whenUserIsValid_postUserToDataBase(){
-        User user = createValidUser();
-        testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
-        assertThat(userRepository.count()).isEqualTo(1);
     }
 }

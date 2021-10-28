3589bbfd30ef7085cad84ca6ce5205512c1e12c2
santosjordi <jordiws@gmail.com>
2021-10-28 20:42:18 -0300

    update

--- hoaxify-backend/src/test/java/com/udemyspringtdd/hoaxifybackend/UserControllerTest.java
+++ hoaxify-backend/src/test/java/com/udemyspringtdd/hoaxifybackend/UserControllerTest.java
@@ -2,7 +2,6 @@ package com.udemyspringtdd.hoaxifybackend;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.udemyspringtdd.hoaxifybackend.user.UserRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,7 +14,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.udemyspringtdd.hoaxifybackend.shared.GenericResponse;
+import com.udemyspringtdd.hoaxifybackend.user.UserRepository;
 import com.udemyspringtdd.hoaxifybackend.user.User;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -47,6 +50,22 @@ public class UserControllerTest {
         User user = createValidUser();
         testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
         assertThat(userRepository.count()).isEqualTo(1);
+    }
+
+    @Test
+    public void postUser_whenUserIsValid_receiveSuccessMessage(){
+        User user = createValidUser();
+        ResponseEntity<GenericResponse> response = testRestTemplate.postForEntity(API_1_0_USERS, user, GenericResponse.class);
+        assertThat(response.getBody().getMessage()).isNotNull();
+    }
+
+    @Test
+    public void postUser_whenUserIsValid_passwordIsHashedInDatabase(){
+        User user = createValidUser();
+        ResponseEntity<Object> response = testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
+        List<User> users = userRepository.findAll();
+        User inDB = users.get(0);
+        assertThat(inDB.getPassword()).isNotEqualTo(user.getPassword());
     }
 
     private User createValidUser() {
---
+++ hoaxify-frontend/src/pages/UserSignupPage.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+export class UserSignupPage extends React.Component {
+    
+    state = {
+        displayName: '',
+        username: '',
+        password: ''
+    };
+
+    onChangeDisplayName = (event) => {
+        const value = event.target.value;
+        this.setState({displayName: value})
+    }
+
+    onChangeUsername = (event) => {
+        const value = event.target.value;
+        this.setState({username: value})
+    }
+
+    onChangePassword = (event) => {
+        const value = event.target.value;
+        this.setState({password: value})
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>Sign Up</h1>
+                <div>
+                    <input 
+                        placeholder="Your display name" 
+                        value={this.state.displayName}
+                        onChange={this.onChangeDisplayName}
+                    />
+                </div>
+                <div>
+                    <input 
+                        placeholder="Your username"
+                        value={this.state.username}
+                        onChange={this.onChangeUsername}
+                    />
+                </div>
+                <div>
+                    <input 
+                        placeholder="Your password" type="password"
+                        value={this.state.password}
+                        onChange={this.onChangePassword}
+                    />
+                </div>
+                <div>
+                    <input placeholder="Repeat your password" type="password" />
+                </div>
+                <div>
+                    <button>Sign Up</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default UserSignupPage;

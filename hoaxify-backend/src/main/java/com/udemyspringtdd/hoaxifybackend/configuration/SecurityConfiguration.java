3cb76647b481a88f106e40f9a1a34657bbfa2da1
santosjordi <jordiws@gmail.com>
2022-01-27 10:17:42 -0300

    31. Login Errors A

--- hoaxify-backend/src/main/java/com/udemyspringtdd/hoaxifybackend/configuration/SecurityConfiguration.java
+++ hoaxify-backend/src/main/java/com/udemyspringtdd/hoaxifybackend/configuration/SecurityConfiguration.java
@@ -1,9 +1,11 @@
 package com.udemyspringtdd.hoaxifybackend.configuration;
 
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -11,11 +13,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.httpBasic();
+
+        // Removes the header "WWW-Authenticate" in the response in case of failure
+        http.httpBasic().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
 
         // Spring checks if the request was authorized before allowing the matching request to continue
-        http
-                .authorizeRequests().antMatchers(HttpMethod.POST, "/api/1.0/login").authenticated()
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/1.0/login").authenticated()
                 .and()
                 .authorizeRequests().anyRequest().permitAll(); //order in which the "authorize" is written matters!
     }

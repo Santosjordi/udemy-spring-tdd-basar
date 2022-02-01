a26db1061d1fd613bd1b7a14f095a507e170078f
santosjordi <jordiws@gmail.com>
2022-01-31 21:38:43 -0300

    37. handling input change

--- hoaxify-frontend/src/pages/LoginPage.spec.js
+++ hoaxify-frontend/src/pages/LoginPage.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { LoginPage } from './LoginPage';
 
 describe('login page', () => {
@@ -29,5 +29,31 @@ describe('login page', () => {
             const button = container.querySelector('button');
             expect(button).toBeInTheDocument();
         });
-    })
+    });
+    describe('interactions', () => {
+        const changeEvent = (content) => {
+            return {
+               target: {
+                value: content
+                } 
+            };
+        };
+        it('sets the username value into state', ()=> {
+            const { queryByPlaceholderText } = render(<LoginPage/>);
+            const usernameInput = queryByPlaceholderText('Your username');
+            fireEvent.change(usernameInput, changeEvent('my-user-name'));
+            expect(usernameInput).toHaveValue('my-user-name');
+        });
+        it('sets the password value into state', ()=> {
+            const { queryByPlaceholderText } = render(<LoginPage/>);
+            const passwordInput = queryByPlaceholderText('Your password');
+            fireEvent.change(passwordInput, changeEvent('P4ssword'));
+            expect(passwordInput).toHaveValue('P4ssword');
+        });
+        it('calls postLogin when actions are provided in props and input fields have value', () =>{
+            const actions = {
+                postLogin: jest.fn().mockResolvedValue({});
+            }
+        });
+    });
 })
